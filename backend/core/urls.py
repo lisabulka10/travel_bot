42d02bcf4f6f957c61d8bb1c5dcd10bb04823f23
lisabulka10 <es.balykova@gmail.com>
2026-02-20 20:19:23 +0300

    add swagger and jwt authentication

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -15,8 +15,29 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+# from rest_framework_simplejwt.views import (
+#     TokenObtainPairView,
+#     TokenRefreshView,
+#     TokenVerifyView
+# )
+
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView
+)
+
+BASE_API_V1_PREFIX = 'api/v1'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path(f'{BASE_API_V1_PREFIX}/', include("authenticate.urls")),
+    #path(f'{BASE_API_V1_PREFIX}/', include("crmlite.urls")),
+
+    path(f'{BASE_API_V1_PREFIX}/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path(f'{BASE_API_V1_PREFIX}/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path(f'{BASE_API_V1_PREFIX}/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc')
 ]
